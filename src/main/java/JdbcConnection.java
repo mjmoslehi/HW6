93196b6e02085ab8c63f6316303233aef53984b9
mamad <you@example.com>
2024-01-10 19:49:25 +0330

    add Connection obj

--- src/main/java/JdbcConnection.java
+++ src/main/java/JdbcConnection.java
@@ -1,2 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    private final Connection connection =
+            DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/postgres",
+                    "postgres",
+                    "mjm6yhn7ujm");
+
+    public JdbcConnection() throws SQLException {
+    }
 }
