ddb03cc296c06af6a853a052aadd1617c7d107d8
mamad <you@example.com>
2024-01-11 06:39:53 +0330

    create Book class with getter and setter methods and constructor but without id

---
+++ src/main/java/Book.java
@@ -0,0 +1,45 @@
+public class Book {
+
+    private int id ;
+    private String title ;
+    private int year;
+    private int AuthorId;
+
+    public Book(String title, int year, int authorId) {
+        this.title = title;
+        this.year = year;
+        AuthorId = authorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getAuthorId() {
+        return AuthorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        AuthorId = authorId;
+    }
+}
