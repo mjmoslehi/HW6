e60fb7922e57a28a600226e349c632cf17b4b24c
mamad <you@example.com>
2024-01-11 06:37:37 +0330

    create Author class with getter and setter methods and constructor but without id and booklist

---
+++ src/main/java/Author.java
@@ -0,0 +1,54 @@
+public class Author {
+
+    private int id ;
+    private String firstname ;
+    private String lastname ;
+    private int age ;
+    private String[] booklist ;
+
+    public Author(String firstname, String lastname, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getBooklist() {
+        return booklist;
+    }
+
+    public void setBooklist(String[] booklist) {
+        this.booklist = booklist;
+    }
+}
